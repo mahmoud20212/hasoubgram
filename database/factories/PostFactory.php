246de10e20f55072faa788761879fc638ab3fe3a
mahmoud20212 <occee123@gmail.com>
2025-08-05 14:07:22 +0300

    Build seed data using factories

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,7 +19,10 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'description' => fake()->sentence(),
+            'image' => 'https://via.placeholder.com/150',
+            'slug' => Str::slug(fake()->sentence(6)),
+            'user_id' => User::factory(),
         ];
     }
 }
